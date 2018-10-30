1efb9a5b426eeed326262270eac56d1b3181e9c1
Advait Pathak <pathakadvait22@gmail.com>
2018-10-30 12:16:50 +0530

    Web services SOAP commit

--- GovernmentSupplyWebProject/src/com/ws/soap/GetAllOrders.java
+++ GovernmentSupplyWebProject/src/com/ws/soap/GetAllOrders.java
@@ -8,6 +8,7 @@ import javax.jws.WebService;
 import com.Service.OrderService;
 import com.al.model.Order;
 
+/*Web service to get all the orders placed by the government client*/
 @WebService(name="GetAllOrders", serviceName="GetAllOrders", portName="newPort") 
 public class GetAllOrders implements GetAllOrdersInterface
 {
